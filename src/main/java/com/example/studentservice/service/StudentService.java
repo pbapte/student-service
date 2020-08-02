3f1f38e96f64411ffb41f7ed33d06d5b5aa60f01
pbapte <prashant.bapte@gmail.com>
2020-08-02 15:04:54 +0530

    Modified to add support for all CRUD operations.

--- src/main/java/com/example/studentservice/service/StudentService.java
+++ src/main/java/com/example/studentservice/service/StudentService.java
@@ -3,10 +3,17 @@ package com.example.studentservice.service;
 import com.example.studentservice.model.Student;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentService {
 
     List<Student> getAllStudents();
 
     void createStudent(Student student);
+
+    void updateStudent(Student student);
+
+    void deleteStudent(Integer id);
+
+    Optional<Student> getStudent(Integer id);
 }
