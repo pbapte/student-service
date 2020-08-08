96804cc7a4c2539ee4d79799ff822a8687024f47
pbapte <prashant.bapte@gmail.com>
2020-08-08 23:07:02 +0530

    modified to add cucumber test.

---
+++ src/test/java/com/example/studentservice/StudentStepdefs.java
@@ -0,0 +1,13 @@
+package com.example.studentservice;
+
+public class StudentStepdefs {
+    public MyStepdefs() {
+        Given("^student service is up$", () -> {
+
+        });
+        When("^user submit get request to fetch all students$", () -> {
+        });
+        Then("^Return all students$", () -> {
+        });
+    }
+}
