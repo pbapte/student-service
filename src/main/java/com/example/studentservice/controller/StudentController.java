3ca15a68628b418eb6d546bcdaabcc0ae00248db
pbapte <37401936+pbapte@users.noreply.github.com>
2023-09-10 22:32:31 +0530

    Update StudentController.java

--- src/main/java/com/example/studentservice/controller/StudentController.java
+++ src/main/java/com/example/studentservice/controller/StudentController.java
@@ -42,7 +42,7 @@ public class StudentController {
 
     @GetMapping("/message")
     public String message() {
-        return "Welcome to Azure!!!";
+        return "Welcome to Azure master!!!";
     }
 
 }
